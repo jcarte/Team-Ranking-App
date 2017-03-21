9933991c21e551d13a944b645615bb25f89cbc7a
Takamon <nicolabirchall@gmail.com>
2017-03-21 21:17:52 +0000

    Front page image scaling

--- TeamRankingApp.Android/PlayerInputActivity.cs
+++ TeamRankingApp.Android/PlayerInputActivity.cs
@@ -19,16 +19,48 @@ namespace TeamRankingApp.Android
     public class PlayerInputActivity : Activity
     {
 
-        GridView gridview;
+        //GridView gridview;
+        ImageButton btnGO;
+        ImageButton btnJC;
+        ImageButton btnLP;
+        ImageButton btnMW;
+        ImageButton btnMP;
+        ImageButton btnN;
+        ImageButton btnNT;
+        ImageButton btnRW;
+
         Button submit;
+        List<Player> allPlayers;
+        List<Player> selectedPlayers;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
             SetContentView(Resource.Layout.PlayerInput);
-            gridview = FindViewById<GridView>(Resource.Id.playerinput_gridview);
-            gridview.Adapter = new PersonAdapter(this,MatchGenerator.GetAllPlayers());
+
+            allPlayers = MatchGenerator.GetAllPlayers();
+            selectedPlayers = new List<Player>();
+
+            btnGO = FindViewById<ImageButton>(Resource.Id.BtnGuy);
+            btnJC = FindViewById<ImageButton>(Resource.Id.BtnJames);
+            btnLP = FindViewById<ImageButton>(Resource.Id.BtnLuca);
+            btnMW = FindViewById<ImageButton>(Resource.Id.BtnMartin);
+            btnMP = FindViewById<ImageButton>(Resource.Id.BtnMegan);
+            btnN = FindViewById<ImageButton>(Resource.Id.BtnNeil);
+            btnNT = FindViewById<ImageButton>(Resource.Id.BtnNicola);
+            btnRW = FindViewById<ImageButton>(Resource.Id.BtnRick);
+
+            btnGO.Click += (s, e) => ButtonPressed("Guy");
+            btnJC.Click += (s, e) => ButtonPressed("James");
+            btnLP.Click += (s, e) => ButtonPressed("Luca");
+            btnMW.Click += (s, e) => ButtonPressed("Martin");
+            btnMP.Click += (s, e) => ButtonPressed("Megan");
+            btnN.Click += (s, e) => ButtonPressed("Neil");
+            btnNT.Click += (s, e) => ButtonPressed("Nicola");
+            btnRW.Click += (s, e) => ButtonPressed("Rick");
+
+            //gridview = FindViewById<GridView>(Resource.Id.playerinput_gridview);
+            //gridview.Adapter = new PersonAdapter(this,MatchGenerator.GetAllPlayers());
             //gridview.SetNumColumns(2);
 
             submit = FindViewById<Button>(Resource.Id.playerinput_submit);
@@ -39,16 +71,31 @@ namespace TeamRankingApp.Android
             //};
         }
 
+        private void ButtonPressed(string name)
+        {
+            //get guy from all players
+            Player p = allPlayers.FirstOrDefault(t => t.Name == name);
+            if (selectedPlayers.Contains(p))
+            {
+                selectedPlayers.Remove(p);
+            }
+            else
+            {
+                selectedPlayers.Add(p);
+            }
+
+        }
+
         private void Submit_Click(object sender, EventArgs e)
         {
-            List<Player> players = ((PersonAdapter)gridview.Adapter).GetSelectedPlayers();
+            //List<Player> players = ((PersonAdapter)gridview.Adapter).GetSelectedPlayers();
 
             //IList<Integer> playInts = players.Select(p => (Integer)p.PlayerID).ToList();
 
             Intent i = new Intent(this, typeof(MatchViewerActivity));
             //i.PutIntegerArrayListExtra("Players", playInts);
 
-            string json = JsonConvert.SerializeObject(players.ToArray());
+            string json = JsonConvert.SerializeObject(selectedPlayers.ToArray());
             i.PutExtra("Players", json);
             StartActivity(i);
         }
